bf71c442d353c0688aeb85524387dbf81d3e7a55
jinbudaily <18336218010@163.com>
2023-08-11 20:42:19 +0800

    deploy

---
+++ assets/column_JavaScript_index.md.d6f65181.js
@@ -0,0 +1,33 @@
+import{_ as n,v as s,b as t,R as o}from"./chunks/framework.e2f4824a.js";const v=JSON.parse('{"title":"JavaScript核心语法","description":"","frontmatter":{},"headers":[],"relativePath":"column/JavaScript/index.md","filePath":"column/JavaScript/index.md"}'),r={name:"column/JavaScript/index.md"},e=o(`<h1 id="javascript核心语法" tabindex="-1">JavaScript核心语法 <a class="header-anchor" href="#javascript核心语法" aria-label="Permalink to &quot;JavaScript核心语法&quot;">​</a></h1><p>✅ <strong>数据类型与堆栈内存</strong>（数据类型，数据类型转换与数据类型检测的几种方法）</p><p>✅ 常用数组方法和字符串方法（数组去重和排序、字符串翻转）</p><p>✅ <strong>函数执行机制与执行上下文</strong>（普通函数与箭头函数、自执行函数、构造函数的执行步骤，执行上下文，执行栈）</p><p>✅ <strong>浏览器垃圾回收机制</strong>及<strong>闭包作用域</strong>（变量提升机制、作用域与作用域链、this、arguments、闭包原理、闭包的应用场景、闭包引发的问题）</p><p>✅ 面向对象深入应用（对象的属性和访问方式，结合<strong>this的指向（call/bind/apply）</strong>、<strong>原型与原型链</strong>、<strong>构造函数</strong>、<strong>new</strong>、<strong>继承</strong>、<strong>深拷贝与浅拷贝</strong>）</p><p>✅ <strong>事件循环与任务队列、微任务、宏任务</strong></p><p>✅ <strong>Promise发展历程</strong></p><p>✅ DOM事件流与事件委托（事件冒泡与事件委托）</p><p>✅ <strong>Ajax</strong>/Fetch/Axios/跨域</p><p>✅ <strong>ES6基础语法和进阶</strong></p><div class="info custom-block"><p class="custom-block-title">INFO</p><pre><code>1. let与const
+2. 解构赋值
+3. 扩展运算符与reset运算符
+4. 模板字符串
+5. 箭头函数
+6. Symbol
+7. Set和Map
+8. Proxy与Reflect
+9.  迭代器和生成器函数
+10. Promise/Async/Await
+11. Class语法
+12. 模块化发展历程(AMD/CMD/CommonJS/ES6Modules)
+</code></pre></div><p>✅ 设计模式（如发布订阅模式，观察者模式）</p><p>✅ 组件插件封装</p><p>✅ <strong>手撕代码</strong></p><div class="tip custom-block"><p class="custom-block-title">TIP</p><pre><code>1. new
+2. call/bind/apply
+3. Promise/Async/Await
+4. reduce/forEach/map/filter
+5. instanceof
+6. 柯里化函数、链式调用
+7. 防抖和节流
+8. Ajax、jsonp
+9. 深浅拷贝
+10. 继承
+11. 数组去重、排序
+12. 数组扁平化
+13. 字符串反转
+14. 千位分隔符
+15. 发布订阅模式
+16. 数据类型检测方法封装
+</code></pre></div><p>✅ 源码解读</p><div class="info custom-block"><p class="custom-block-title">INFO</p><pre><code>1. JQuery
+2. Lodash
+3. underscore
+4. Axios
+</code></pre></div><p>✅ 正则表达式</p>`,19),p=[e];function a(c,i,l,d,g,m){return s(),t("div",null,p)}const h=n(r,[["render",a]]);export{v as __pageData,h as default};
---
+++ assets/column_算法_index.md.65f46f2a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,v as t,b as n}from"./chunks/framework.e2f4824a.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"column/算法/index.md","filePath":"column/算法/index.md"}'),a={name:"column/算法/index.md"};function s(r,o,c,d,i,m){return t(),n("div")}const f=e(a,[["render",s]]);export{p as __pageData,f as default};
